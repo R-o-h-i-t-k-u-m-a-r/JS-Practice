e7ff66c4e410ac3e12b60f0ce10a4ae165c79c3a
Rohit kumar <67084175+R-o-h-i-t-k-u-m-a-r@users.noreply.github.com>
2024-10-05 16:39:10 +0000

    comparition

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -3,6 +3,6 @@
 // console.log("02"==2)
 // console.log("02">1)
 
-console.log(null>0)
+console.log(null>0) //here null is treaing as 0
 console.log(null==0)
-console.log(null>=0)
+console.log(null>=0) //comparison operator convert the value here null is treating as 0
