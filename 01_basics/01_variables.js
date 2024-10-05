d81b0f5d2486adcca6389f70dc78d9c6416adc50
Rohit kumar <67084175+R-o-h-i-t-k-u-m-a-r@users.noreply.github.com>
2024-10-05 14:10:08 +0000

    variabls

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const name = "rohit"
+let age = 25
+var city = "koderma" //not recommend to use due to block scope problem
+userName = "rohitkumar" // we can use like this to create variable but not recomandable
+let passion // without initializing this will contain undefined
+
+console.log(name) //this is used to display one variable
+
+console.table([name,age,city,userName,passion]) //this is used to display all variables in tabular form
+
