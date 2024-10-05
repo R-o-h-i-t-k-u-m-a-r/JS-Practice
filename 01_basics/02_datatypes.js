ba2bbac1e8fa618190234859a0e946274dfade78
Rohit kumar <67084175+R-o-h-i-t-k-u-m-a-r@users.noreply.github.com>
2024-10-05 14:18:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+let name = "rohit"
+let age = 25
+let flag = true
+let num = 2.4
+
+//premitvie datatypes
+//number =>  range 2 to power 53
+//string => ""
+//boolean => true or false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//dedrived dataytpe
+//object
+
+console.log(typeof null);
+console.log(typeof undefined)
+console.log(typeof {})
