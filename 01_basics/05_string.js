e63efeb49137d25e88b62436f75926c2ec80192a
Rohit kumar <67084175+R-o-h-i-t-k-u-m-a-r@users.noreply.github.com>
2024-10-05 18:40:02 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,9 @@
+let name = "rohit"
+console.log(`my name is ${name} kumar`) //string interpolation way
+console.log(name[0])
+console.log(name.charAt(0))
+console.log(name.toUpperCase())
+console.log(name.replace('9','0'))
+console.log(name.length)
+console.log(name.substring(0,4))
+console.log(name.slice(-3,4))
