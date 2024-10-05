8027e637b63b5ddfc847e95b4029691e00586afc
Rohit kumar <67084175+R-o-h-i-t-k-u-m-a-r@users.noreply.github.com>
2024-10-05 15:57:51 +0000

    convertionoperation

---
+++ 01_basics/03_convertionOperations.js
@@ -0,0 +1,19 @@
+// let num = ""
+// console.log( num , typeof(num))
+// let val = Number(num)
+// console.log(val,typeof(val))
+
+// *********************** Operations ***********************
+// let value = 4
+// value = -4;
+// console.log(value)
+
+// let first = "rohit"
+// let last = "kumar"
+// let name = first+last
+// console.log(name)
+
+console.log("1"+2)
+console.log(2+"1")
+console.log(2+2+"1")
+console.log("1"+2+2)
