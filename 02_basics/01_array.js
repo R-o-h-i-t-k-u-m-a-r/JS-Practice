b9eeafe0732b19a8bb937e2cdb9e7a5574400b67
Rohit kumar <67084175+R-o-h-i-t-k-u-m-a-r@users.noreply.github.com>
2024-10-06 15:31:55 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,27 @@
+const myArr = [1,2,3,4,5]
+// console.log(myArr);
+// console.log(typeof(myArr))
+// myArr.push(6)
+// console.log(myArr)
+// myArr.pop()
+// console.log(myArr)
+// myArr.unshift(0)
+// console.log(myArr)
+// myArr.shift()
+// console.log(myArr)
+
+// console.log(myArr.includes(5))
+
+// let newArr = myArr.join();
+// console.log(newArr)
+// console.log(myArr)
+// console.log(typeof(newArr))
+
+//slice and splice
+console.log("A",myArr)
+const myArr1 = myArr.slice(1,3)
+console.log(myArr1)
+console.log("B",myArr)
+const myArr2 = myArr.splice(1,3);
+console.log(myArr2)
+console.log("C",myArr)
