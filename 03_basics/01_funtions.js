187022e914c4d8be6111e10b14e7082d47d8b2d3
Rohit kumar <67084175+R-o-h-i-t-k-u-m-a-r@users.noreply.github.com>
2024-10-06 18:15:00 +0000

    functions

---
+++ 03_basics/01_funtions.js
@@ -0,0 +1,31 @@
+function myName(){
+    console.log("My name is Rohit kumar");
+    
+}
+// myName()
+
+// function addTwoNumber(number1, number2){
+//     console.log(number1+number2)
+// }
+// addTwoNumber(9,8)
+function addTwoNumber(number1,number2){
+    return number1+number2
+}
+// console.log(addTwoNumber(3,4))
+
+// function calcualteCartPrice(value1,value2,...items){
+//     console.log(value1,value2, items)
+// }
+// calcualteCartPrice(1,2,3,4)
+
+function func(obj){
+    console.log(obj)
+}
+
+let myObj = {name:"rohit",age:25}
+func(myObj)
+
+function func2(arr){
+    console.log(arr)
+}
+func2([1,2,3,4])
