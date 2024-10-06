49ec04c9a1145c16ff54737f177b65d5cca0bff0
Rohit kumar <67084175+R-o-h-i-t-k-u-m-a-r@users.noreply.github.com>
2024-10-06 13:57:37 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,15 @@
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(typeof myDate)
+
+// let myCreatedDate = new Date(2024,10,6)
+// console.log(myCreatedDate.toDateString())
+
+// let myCreatedDate = new Date("10-06-2024")
+// console.log(myCreatedDate.toDateString())
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
