08e7eb12ba0fa0f7a29341b1211d27e112334004
Rohit kumar <67084175+R-o-h-i-t-k-u-m-a-r@users.noreply.github.com>
2024-10-06 15:38:39 +0000

    array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,19 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+// const newONe = marvel_heros.concat(dc_heros)
+// console.log(newONe)
+
+//const all_new_heros = [...marvel_heros, ...dc_heros]
+// console.log(all_new_heros)
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// const newOne = another_array.flat(Infinity)
+// console.log(newOne)
+
+console.log(Array.isArray("Rohit"))
+console.log(Array.from("Rohit"))
+console.log(Array.of(1,2,3,4))
+
